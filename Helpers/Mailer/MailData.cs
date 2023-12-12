3a09146e62a7c3d1e4148a5638fb342bf152e9c1
DoMinhQuan <doquan23032003@gmail.com>
2023-12-13 00:48:57 +0700

    prepare for mail service

--- Helpers/Mailer/MailData.cs
+++ Helpers/Mailer/MailData.cs
@@ -1,1 +1,10 @@
-﻿
+﻿namespace pos.Helpers.Mailer
+{
+    public class MailData
+    {
+        public required string EmailToId { get; set; }
+        public required string EmailToName { get; set; }
+        public required string EmailSubject { get; set; }
+        public required string EmailBody { get; set; }
+    }
+}
