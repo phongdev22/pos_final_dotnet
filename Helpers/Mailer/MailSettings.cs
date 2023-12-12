0aef445418a923146ea49960a62a366f9108dde7
DoMinhQuan <doquan23032003@gmail.com>
2023-12-13 00:47:56 +0700

    helper mail service

--- Helpers/Mailer/MailSettings.cs
+++ Helpers/Mailer/MailSettings.cs
@@ -1,12 +1,1 @@
-﻿namespace pos.Helpers.Mailer
-{
-    public class MailSettings
-    {
-        public int Port { get; set; }
-        public required string Server { get; set; }
-        public required string SenderName { get; set; }
-        public required string SenderEmail { get; set; }
-        public required string UserName { get; set; }
-        public required string Password { get; set; }
-    }
-}
+﻿
