ac874a67d2f1178625472055666261e02058a26e
DoMinhQuan <doquan23032003@gmail.com>
2023-12-19 00:47:13 +0700

    add entities

---
+++ Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pos.Entities
+{
+	public class Order
+	{
+		public int Id { get; set; }
+		[Required]
+		public string OrderId { get; set; }
+		public decimal Total { get; set; }
+		public bool Status { get; set; } = false;
+		public decimal? GivenMoney { get; set; }
+
+		public DateTime? DateCreation { get; set; }
+		public virtual Customer Customer { get; set; }
+		public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+		public virtual ApplicationUser? User { get; set; }
+		public virtual RetailStore? RetailStore { get; set; }
+		
+	}
+}
